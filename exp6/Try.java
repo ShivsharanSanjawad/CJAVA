e37d0b4b6db619f883598c92279d8e737252c8cb
ShivsharanSanjawad <shivsharan.sanjawad@gmail.com>
2024-03-14 00:18:21 +0530

    finalizer

---
+++ exp6/Try.java
@@ -0,0 +1,23 @@
+package com.exp6;
+
+class Demo {
+    int a;
+//    String s;
+//
+//    Demo(String s) {
+//        this.s = s;
+//    }
+    Demo(){}
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Object destroyed");
+    }
+}
+public class Try {
+    public static void main(String[] args) {
+        for(int i = 0 ; i<1000000 ; i++)
+        {
+            Demo d = new Demo() ;
+        }
+    }
+}
